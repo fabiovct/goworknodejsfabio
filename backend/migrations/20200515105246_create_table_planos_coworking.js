1d255c087659d2aced92ae8b8bea480e2817bce5
Fábio Vicentini <vct.fabio.vct@gmail.com>
2020-05-19 10:37:46 -0300

    private routes front end

--- backend/migrations/20200515105246_create_table_planos_coworking.js
+++ backend/migrations/20200515105246_create_table_planos_coworking.js
@@ -2,7 +2,7 @@ exports.up = function(knex) {
     return knex.schema.createTable('planos_coworking', table =>{
         table.increments('id_plano').primary()
         table.string('nome_plano').notNull().unique()
-        table.float('valor_mensal', 2,2).notNull()
+        table.float('valor_mensal', 4,2).notNull()
     })
 };
 
--- frontend/src/routes.js
+++ frontend/src/routes.js
@@ -1,4 +1,4 @@
-import { Switch, Route } from 'react-router-dom';
+import { Switch, Route, Redirect } from 'react-router-dom';
 import Login from './pages/login';
 import Home from './pages/home';
 
@@ -18,36 +18,42 @@ import EditEmployee from './pages/customers/edit-employee';
 
 import React from 'react';
 
-
    function Routes() {
-      
-            
+    const PrivateRoute = ({ component: Component, ...rest }) => (
+        <Route
+          {...rest}
+          render={props =>
+            localStorage.getItem('status') ? (
+              <Component {...props} />
+            ) : (
+              <Redirect to={{ pathname: "/", state: { from: props.location } }} />
+            )
+          }
+        />
+      );
+             
     return (
       
             <Switch>
                 <Route path="/" exact component={Login}/>    
-                <Route path="/home" component={Home}/>
+                <PrivateRoute path="/home" component={Home}/>
 
-                <Route path="/offices" exact component={Offices}/>
-                <Route path="/offices/new" exact component={NewOffices}/>
-                <Route path="/offices/:id" exact component={EditOffices}/>
+                <PrivateRoute path="/offices" exact component={Offices}/>
+                <PrivateRoute path="/offices/new" exact component={NewOffices}/>
+                <PrivateRoute path="/offices/:id" exact component={EditOffices}/>
 
-                <Route path="/schemes" exact component={Schemes}/>
-                <Route path="/schemes/new" exact component={NewSchemes}/>
-                <Route path="/schemes/:id" exact component={EditSchemes}/>
+                <PrivateRoute path="/schemes" exact component={Schemes}/>
+                <PrivateRoute path="/schemes/new" exact component={NewSchemes}/>
+                <PrivateRoute path="/schemes/:id" exact component={EditSchemes}/>
 
-                <Route path="/customers" exact component={Customers}/>
-                <Route path="/customers/new" exact component={NewCustomers}/>
-                <Route path="/customers/:id" exact component={EditCustomers}/>
-                <Route path="/customers/employees/new/:id" exact component={NewEmployee}/>
-                <Route path="/customers/employees/edit/:id_usuario/:id_cliente" exact component={EditEmployee}/>
-
-
-
+                <PrivateRoute path="/customers" exact component={Customers}/>
+                <PrivateRoute path="/customers/new" exact component={NewCustomers}/>
+                <PrivateRoute path="/customers/:id" exact component={EditCustomers}/>
+                <PrivateRoute path="/customers/employees/new/:id" exact component={NewEmployee}/>
+                <PrivateRoute path="/customers/employees/edit/:id_usuario/:id_cliente" exact component={EditEmployee}/>
             </Switch>
         
     );
 }
-
 
 export default Routes;
